3b482fa7fc2a7d1235874db8224fdecb2df02688
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-12-13 07:58:15 -0500

    update page-template for module exports

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,4 @@
-const generatePage = (name, github) => {
+module.exports = (name, github) => {
     return `
     <!DOCTYPE html> 
     <html lang="en"> 
@@ -17,4 +17,4 @@ const generatePage = (name, github) => {
     `;
   };
 
-  module.exports = generatePage;
+  
