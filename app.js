ff5c856ef485ab68aec4e1b6ce71b234ac80bd98
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-12-13 07:52:02 -0500

    generate html file

---
+++ app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = require('./src/page-template.js');
+
+
+
+
+  fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+      console.log('Portfolio complete! Check out index.html to see the output!');
+  });
