64ce7ea31382ac26d0b666524a3c79163d917ce0
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-12-14 17:36:26 -0500

    using inquirer

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
 
 
+//   fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw new err;
 
-  fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-      console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//       console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
