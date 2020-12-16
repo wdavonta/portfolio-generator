14ba00550a9e807fff96e6e5da3c36522ec1c47e
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-12-15 22:40:32 -0500

    update page template js main projectarr

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const inquirer = require('inquirer');
 
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -134,19 +137,17 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+
+     fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see t!');
+  });
   });
   
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
 
 
 
-//   fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw new err;
-
-//       console.log('Portfolio complete! Check out index.html to see the output!');
-//   });
