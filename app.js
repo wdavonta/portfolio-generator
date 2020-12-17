e683e62b47b9bf829394c39d7c3b2ee0b8cb1a3a
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-12-17 17:31:43 -0500

    added promises

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
-
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+const {writeFile, copyFile} = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -130,34 +129,25 @@ Add a New Project
     } else {
       return portfolioData;
     }
-  })
+  });
 };
   
 
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-
-     fs.writeFile('./dist/index.html', pageHTML, err => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-      console.log('Page created! Check out index.html in this directory to see t!');
-
-      fs.copyFile('./src/style.css', './dist/style.css', err => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log('Style sheet copied successfully!');
-      });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-  
-
-
-});
-
